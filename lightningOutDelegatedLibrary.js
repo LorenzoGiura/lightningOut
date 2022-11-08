6a6df1f0354558a9a675f21edb8cc414f3409f4d
Sagit23 <63496669+Sagit23@users.noreply.github.com>
2022-11-08 16:48:25 +0100

    Update lightningOutDelegatedLibrary.js

--- lightningOutDelegatedLibrary.js
+++ lightningOutDelegatedLibrary.js
@@ -149,7 +149,7 @@ $Lightning._delegate = (function() {
             _previousRequestAuthToken = authToken;
         }
         xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-        console.log('guarda qui'+ Json.stringify(xhr));
+        console.log('guarda qui'+ JSON.stringify(xhr));
         xhr.send();
     };
 
