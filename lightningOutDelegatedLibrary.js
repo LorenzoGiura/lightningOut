295ba124eb09aca475e4d34813a995af29370854
Lorenzo Giura <102514603+LorenzoGiura@users.noreply.github.com>
2022-11-08 15:19:44 +0100

    Update lightningOutDelegatedLibrary.js

--- lightningOutDelegatedLibrary.js
+++ lightningOutDelegatedLibrary.js
@@ -173,7 +173,7 @@ $Lightning._delegate = (function() {
                 requestApp(applicationTag, lightningEndPointURI, authToken, paramsObj, function(xhr) {
                     var errorMarker = xhr.responseText.indexOf("/*ERROR*/");
                     if (errorMarker == -1) {
-                        var config = xhr.responseText;
+                        var config = JSON.parse(xhr.responseText);
 
                         // save the delegate version to local storage
                         try {
